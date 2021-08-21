b5d3dc194ad63d817604a72efb59147e2d986a0c
Cathy Richardson <cathyanne08@gmail.com>
2021-08-21 17:11:13 -0600

    completed step 6

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -10,19 +10,21 @@ import Nav from './../shared/Nav';
 import './Budget.css';
 // IMPORT THE ACTION CREATOR FROM THE REDUCER FILE FOR USE BY THE CONNECT METHOD, WHICH THEN ADDS THE FUNCTION TO THE PROPS // OBJECT OF THIS COMPONENT
 import { requestUserData } from '../../ducks/userReducer';
+import { requestBudgetData } from '../../ducks/budgetReducer';
 
 
 class Budget extends Component {
   componentDidMount() {
     // WHEN THE COMPONENT MOUNTS, THE ACTION CREATOR IS INVOKED, THE REDUCER FUNCTION FIRES, AND STATE IS UPDATED ACCORDINGLY   // IN THE REDUX STORE
     this.props.requestUserData();
+    this.props.requestBudgetData();
   }
 
   render() {
     //DESTRUCTURE THE LOADING PROPERTY FROM THE BUDGET OBJECT THAT WAS MAPPED TO PROPS THROUGH MAPSTATETOPROPS/CONNECT
-    const { loading } = this.props.budget;
+    const { loading, purchases, budgetLimit } = this.props.budget;
     const { fistName, lastName } = this.props.user;
-    return (
+     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
@@ -30,11 +32,11 @@ class Budget extends Component {
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases purchases={purchases}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit}/>
+              <Chart2 purchases={purchases}/>
             </div>
           </div>
         </div>
@@ -55,4 +57,4 @@ function mapStateToProps(state) {
 // THE CONNECT METHOD TAKES IN THE MAPSTATETOPROPS FUNCTION AND CONNECTS THIS COMPONENT TO THE REDUX STORE
 // IN ORDER TO ACCESS THE REQUESTUSERDATA ACTION CREATOR, YOU NEED TO CONNECT IT TO
 // THE REDUCER FUNCTION THROUGH THE CONNECT METHOD. THE CONNECT METHOD ACCEPTS TWO ARGUMENTS, A MAPSTATETOPROPS OBJECT, AND A MAPDISPATCHTOPROPS OBJECT. OUR DISPATCHED ACTIONS GO INSIDE OF THE SECOND ARGUMENT OBJECT AS A KEY/VALUE PAIR. 
-export default connect(mapStateToProps, { requestUserData })(Budget);
+export default connect(mapStateToProps, { requestUserData, requestBudgetData })(Budget);
--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 
 //sets initial state for the store
 const initialState = {
@@ -6,6 +7,35 @@ const initialState = {
     loading: false
 }
 
+//The base action types
+const REQUEST_BUDGET_DATA = "REQUEST_BUDGET_DATA";
+
+// Action Creator
+export function requestBudgetData() {
+    let data = axios.get('/api/budget-data').then(({ data }) => data)
+    return {
+        type: REQUEST_BUDGET_DATA,
+        payload: data
+    }
+}
+
+
 export default function reducer(state = initialState, action) {
-    return state;
+    switch (action.type) {
+        case REQUEST_BUDGET_DATA + "_PENDING":
+            // IN ORDER TO KEEP VALUES PREVIOUSLY STORED ON STATE, WE SPREAD THE CURRENT STATE OBJECT
+            // INTO THE RETURNED OBJECT AND UPDATE ONLY THE VALUES IN STATE WE WANT TO CHANGE
+            return {
+                ...state,
+                loading: true
+            }
+        case REQUEST_BUDGET_DATA + "_FULFILLED":
+            return {
+                ...state,
+                ...action.payload,
+                loading: false
+            }
+        default: return state;
+    }
+
 }
